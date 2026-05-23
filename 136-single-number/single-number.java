86c34fafb13d12248ad19c96f6df6a6907ab05b6
Hatice99 <73390376+Hatice99@users.noreply.github.com>
2026-05-23 17:51:29 +0200

    Time: 10 ms (26.42%) | Memory: 47.6 MB (5.11%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,8 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
+
         int len = nums.length;
         for(int i = 0; i < len; i+=2){
+            //if only one element or if it not equal return the left or if it is the last element
             if(len==1 || i == len -1 || nums[i]!=nums[i+1]){
                 return nums[i];
 
