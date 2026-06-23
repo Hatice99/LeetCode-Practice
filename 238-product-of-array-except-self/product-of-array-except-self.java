b7dd6ba6fc98a3f1c0dc7d6e38155e287f8e5096
Hatice99 <73390376+Hatice99@users.noreply.github.com>
2026-06-23 15:22:16 +0200

    Time: 2 ms (94.59%) | Memory: 71.9 MB (57.82%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix_array=new int [nums.length];
-        int[] suffix_array=new int [nums.length];
 
-        int[] answer_array=new int [nums.length];
+        int[] answer_array = new int[nums.length];
 
-        //calculation of prefix array
-        prefix_array[0] = 1;
-        for(int i=1; i<nums.length; i++){
-            prefix_array[i] = prefix_array[i-1] * nums[i-1]; 
+        answer_array[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            answer_array[i] = answer_array[i - 1] * nums[i - 1];
         }
 
-        //calculation of suffix array
-        suffix_array[nums.length-1] = 1;
-        for(int i=nums.length-2; i >=0; i--){
-            suffix_array[i] = suffix_array[i+1] * nums[i+1];
-        }
+        int value = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            value = value * nums[i + 1];
+            answer_array[i] = answer_array[i] * value;
 
-        for(int i=0; i<nums.length; i++){
-            answer_array[i] = prefix_array[i] * suffix_array[i];
         }
 
         return answer_array;
-        
+
     }
 }
