7d8e21cfe1fba71bbdbc6d8ef1fbe6f49b390a0f
Hatice99 <73390376+Hatice99@users.noreply.github.com>
2026-08-03 18:03:00 +0200

    Time: 0 ms (100.00%) | Memory: 48.1 MB (85.80%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,9 @@
+import java.util.Collections;
+class Solution {
+    public int search(int[] nums, int target) {
+        int index = Arrays.binarySearch(nums, target);
+        if(index >=0) return index;
+        return -1;
+        
+    }
+}
